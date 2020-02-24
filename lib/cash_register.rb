4e8c4a2ffa24e6136a8ed541a76f827a60aba10c
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-24 23:41:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,11 +1,11 @@
 class CashRegister
-  attr_accessor :total, :discount, :price, :items, :last_transaction
+  attr_accessor :total, :discount, :price, :items
 
   def initialize(discount = 0)
     @total = 0
     @discount = discount
     @items = []
-    @last_transaction = []
+  
   end
 
   def add_item(item, price, quantity = 1)
