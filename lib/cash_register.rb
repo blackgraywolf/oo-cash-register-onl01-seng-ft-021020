0f1d37ebcc8345a22763f9c64411475d35f0c2a4
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-24 23:50:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,12 +5,12 @@ class CashRegister
     @total = 0
     @discount = discount
     @items = []
-  
   end
 
   def add_item(item, price, quantity = 1)
     @price = price
     @total += price * quantity
+    
     if quantity > 1
       counter = 0
       while counter < quantity
